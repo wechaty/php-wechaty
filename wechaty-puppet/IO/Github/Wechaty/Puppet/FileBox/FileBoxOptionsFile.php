24eae7b3948018ab0783bedcb08f23c4b3dd1c63
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-07-23 11:57:41 +0800

    add filebox options

--- wechaty-puppet/IO/Github/Wechaty/Puppet/FileBox/FileBoxOptionsFile.php
+++ wechaty-puppet/IO/Github/Wechaty/Puppet/FileBox/FileBoxOptionsFile.php
@@ -10,5 +10,17 @@ namespace IO\Github\Wechaty\Puppet\FileBox;
 
 
 class FileBoxOptionsFile {
+    public $type;
+    public $path;
+    public $name;
 
+    public function __construct($type, $path, $name) {
+        $this->type = $type;
+        $this->path = $path;
+        $this->name = $name;
+    }
+
+    public function __toString() {
+        return "FileBoxOptionsFile(type=$this->type, path='$this->path')";
+    }
 }
