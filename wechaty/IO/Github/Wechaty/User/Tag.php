3d920522475edc9763201aa92b676f7908bd3fb2
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-07-24 20:31:17 +0800

    add contact avatar method

---
+++ wechaty/IO/Github/Wechaty/User/Tag.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: peterzhang
+ * Date: 2020/7/24
+ * Time: 8:31 PM
+ */
+namespace IO\Github\Wechaty\User;
+
+use IO\Github\Wechaty\Accessory;
+
+class Tag extends Accessory {
+
+}
