7419f598f68dc960125d339b44744bdf976fb707
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-07-24 20:02:07 +0800

    add message url link method

--- wechaty/IO/Github/Wechaty/User/Message.php
+++ wechaty/IO/Github/Wechaty/User/Message.php
@@ -301,7 +301,7 @@ class Message extends Accessory {
         return $contact;
     }
 
-    function toUrlLink() : UrlLink { // video?
+    function toUrlLink() : UrlLink {
         if($this->type() != MessagePayload::MESSAGETYPE_URL) {
             throw new WechatyException("message not a Url Link");
         }
