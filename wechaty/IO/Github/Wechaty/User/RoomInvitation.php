024a65264b9507c84ba939fb78204d34aa78db89
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-07-27 21:03:01 +0800

    add room invitation accept method

--- wechaty/IO/Github/Wechaty/User/RoomInvitation.php
+++ wechaty/IO/Github/Wechaty/User/RoomInvitation.php
@@ -8,10 +8,31 @@
 namespace IO\Github\Wechaty\User;
 
 use IO\Github\Wechaty\Accessory;
+use IO\Github\Wechaty\Util\Logger;
 
 class RoomInvitation extends Accessory {
     public function __construct($wechaty, $id = "") {
         $this->_id = $id;
         parent::__construct($wechaty);
+    }
+
+    function accept() {
+        $this->wechaty->getPuppet()->roomInvitationAccept($this->_id);
+
+        $inviter = $this->inviter();
+        $topic = $this->topic();
+
+        Logger::DEBUG("accept", array("topic" => $topic, "inviter" => $inviter));
+        $inviter->ready();
+    }
+
+    function inviter():Contact{
+        $payload = $this->wechaty->getPuppet()->roomInvitationPayload($this->_id);
+        return $this->wechaty->contactManager->load($payload->inviterId);
+    }
+
+    function topic():String {
+        $payload = $this->wechaty->getPuppet()->roomInvitationPayload($this->_id);
+        return $payload->topic ?: "";
     }
 }
