7408fa317b6ad78902c688bd1a1689f053ebc8f4
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-07-31 22:49:25 +0800

    add friendship method

--- wechaty/IO/Github/Wechaty/User/Manager/FriendshipManager.php
+++ wechaty/IO/Github/Wechaty/User/Manager/FriendshipManager.php
@@ -8,10 +8,24 @@
 namespace IO\Github\Wechaty\User\Manager;
 
 use IO\Github\Wechaty\Accessory;
+use IO\Github\Wechaty\Puppet\Schemas\Query\FriendshipSearchCondition;
+use IO\Github\Wechaty\User\Contact;
 use IO\Github\Wechaty\User\Friendship;
+use IO\Github\Wechaty\Util\Logger;
 
 class FriendshipManager extends Accessory {
     function load(String $id): Friendship {
         return new Friendship($this->wechaty, $id);
+    }
+
+    function search(FriendshipSearchCondition $queryFilter): ?Contact {
+        Logger::DEBUG("FriendshipManager search", array("query" => $queryFilter));
+        $contactId = $this->wechaty->getPuppet()->friendshipSearch($queryFilter);
+        if(empty($contactId)) {
+            return null;
+        }
+        $contact = $this->wechaty->contactManager->load($contactId);
+        $contact->ready();
+        return $contact;
     }
 }
