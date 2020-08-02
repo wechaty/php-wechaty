69a0f2fe959379297169fb72520e618235f9259d
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-08-02 20:03:36 +0800

    add message method

--- wechaty-puppet/IO/Github/Wechaty/Puppet/Schemas/Query/AbstractQueryFilter.php
+++ wechaty-puppet/IO/Github/Wechaty/Puppet/Schemas/Query/AbstractQueryFilter.php
@@ -7,39 +7,14 @@
  */
 namespace IO\Github\Wechaty\Puppet\Schemas\Query;
 
+use IO\Github\Wechaty\Puppet\Util\ReflectionUtil;
+
 abstract class AbstractQueryFilter {
     public static function getProperties($class) {
-        $ref = new \ReflectionClass($class);
-        $props = $ref->getProperties();
-        $properties = array();
-        foreach ($props as $key => $val) {
-            if($val->isPublic()) {
-                $properties[] = $val->getName();
-            }
-        }
-
-        return $properties;
+        return ReflectionUtil::getPublicProperties($class);
     }
 
     public static function reflection($class) {
-        $ref = new \ReflectionClass($class);
-
-        $consts = $ref->getConstants(); //返回所有常量名和值
-        echo "----------------consts:---------------" . PHP_EOL;
-        foreach ($consts as $key => $val)  {
-            echo "$key : $val" . PHP_EOL;
-        }
-
-        $props = $ref->getDefaultProperties(); //返回类中所有属性
-        echo "--------------------props:--------------" . PHP_EOL . PHP_EOL;
-        foreach ($props as $key => $val) {
-            echo "$key : $val" . PHP_EOL;  // 属性名和属性值
-        }
-
-        $methods = $ref->getMethods();   //返回类中所有方法
-        echo "-----------------methods:---------------" . PHP_EOL . PHP_EOL;
-        foreach ($methods as $method) {
-            echo $method->getName() . PHP_EOL;
-        }
+        ReflectionUtil::reflection($class);
     }
 }
