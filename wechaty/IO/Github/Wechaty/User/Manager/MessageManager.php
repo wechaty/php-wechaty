78dbff9de946a6155dc5fa58587fde3beec910a1
zhangchunsheng <zhangchunsheng423@gmail.com>
2020-08-02 20:45:46 +0800

    add message method

--- wechaty/IO/Github/Wechaty/User/Manager/MessageManager.php
+++ wechaty/IO/Github/Wechaty/User/Manager/MessageManager.php
@@ -38,4 +38,26 @@ class MessageManager extends Accessory {
 
         return $messageList[0];
     }
+
+    function findAll(MessageQueryFilter $query) : array {
+        Logger::DEBUG("findAll({})", array("query" => $query));
+        try {
+            $messageIdList = $this->wechaty->getPuppet()->messageSearch($query);
+            $that = $this;
+            $messageList = array_map(function($value) use ($that) {
+                return $that->load($value);
+            }, $messageIdList);
+            try {
+                foreach($messageList as $value) {
+                    $value->ready();
+                }
+                return $messageList;
+            } catch(\Exception $e) {
+                Logger::WARNING("findAll() message.ready() rejection: {}", $e->getTrace());
+            }
+        } catch (\Exception $e){
+            Logger::WARNING("findAll() rejected: {}", $e->getTrace());
+        }
+        return array();
+    }
 }
