03c925e38bca8f2d1e5ddb65e4094b51a0135c3d
zhangchunsheng <zhangchunsheng423@gmail.com>
2021-11-24 16:07:31 +0800

    add wechaty ca, see #16

--- wechaty-puppet-service/IO/Github/Wechaty/PuppetService/PuppetService.php
+++ wechaty-puppet-service/IO/Github/Wechaty/PuppetService/PuppetService.php
@@ -26,6 +26,7 @@ use IO\Github\Wechaty\PuppetService\Auth\WechatyCA;
 use IO\Github\Wechaty\PuppetService\Exceptions\PuppetServiceException;
 use IO\Github\Wechaty\Util\Console;
 use IO\Github\Wechaty\Util\Logger;
+use LM\Exception;
 use Wechaty\Puppet\ContactPayloadResponse;
 use Wechaty\Puppet\EventResponse;
 use Wechaty\Puppet\EventType;
@@ -797,18 +798,23 @@ class PuppetService extends Puppet {
         // WECHATY_PUPPET_SERVICE_TLS_CA_CERT
         // WECHATY_PUPPET_SERVICE_TLS_SERVER_NAME
         $noTls = getenv("WECHATY_PUPPET_SERVICE_NO_TLS_INSECURE_CLIENT");
-        if($noTls === "true" || $noTls === true) {
-            Logger::DEBUG("start client with no tls");
-            $this->_grpcClient = new \Wechaty\PuppetClient($hostname, [
-                'credentials' => \Grpc\ChannelCredentials::createInsecure(),
-                'update_metadata' => $updateMetadata,
-            ]);
-        } else {
-            Logger::DEBUG("start client with tls");
-            $this->_grpcClient = new \Wechaty\PuppetClient($hostname, [
-                'credentials' => \Grpc\ChannelCredentials::createSsl(WechatyCA::TLS_CA_CERT),
-                'update_metadata' => $updateMetadata,
-            ]);
+        try {
+            if($noTls === "true" || $noTls === true) {
+                Logger::DEBUG("start client with no tls");
+                $this->_grpcClient = new \Wechaty\PuppetClient($hostname, [
+                    'credentials' => \Grpc\ChannelCredentials::createInsecure(),
+                    'update_metadata' => $updateMetadata,
+                ]);
+            } else {
+                Logger::DEBUG("start client with tls");
+                $this->_grpcClient = new \Wechaty\PuppetClient($hostname, [
+                    'credentials' => \Grpc\ChannelCredentials::createSsl(WechatyCA::TLS_CA_CERT),
+                    'update_metadata' => $updateMetadata,
+                ]);
+            }
+        } catch (\Exception $e) {
+            Logger::ERR("start client error");
+            Logger::ERR($e);
         }
 
         return $this->_grpcClient;
